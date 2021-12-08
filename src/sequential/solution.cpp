c8753f073603465881cf3d30ad1c9e1769350e5a
Ziteng Shu <shuziteng1998@gmail.com>
2021-12-08 15:28:07 -0500

    delete update_pheromone in solution

--- src/sequential/solution.cpp
+++ src/sequential/solution.cpp
@@ -25,16 +25,6 @@ Solution &Solution::operator=(const Solution &solution) {
     return *this;
 }
 
-void Solution::update_pheromone(double **phero, double Q, Dataloader *dataloader) {
-    int max_itr = path.n_cities;
-    for (int i = 1; i < max_itr; ++i) {
-        int start = path.route[i-1];
-        int end = path.route[i];
-        phero[start][end] +=  Q / length;
-        phero[end][start] = phero[start][end];
-    }
-}
-
 void Solution::reset() {
     length = static_cast<double>(INT_MAX);
 }
--- src/sequential/solution.h
+++ src/sequential/solution.h
@@ -15,7 +15,6 @@ public:
     Solution(const Solution &solution);
     Solution& operator= (const Solution& solution);
     bool operator< (Solution& solution);
-    void update_pheromone(double **phero, double Q, Dataloader *dataloader);
     virtual ~Solution();
     void reset();
     double length;
