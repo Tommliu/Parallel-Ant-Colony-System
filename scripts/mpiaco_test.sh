81050bae1670f08ed96c9e3f7729de8a2e791b35
Minghao Liu <tommliu96@gmail.com>
2021-12-09 04:29:06 +0000

    mpiaco shell script

--- scripts/mpiaco_test.sh
+++ scripts/mpiaco_test.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
-rm ./output/*/*_profile
+# rm ./output/*/*_profile
+total_iteration=256
 for i in 1 2 4 8 16
 do
-  mpirun -np $i ./build/bin/aco -f tests/xqf131.tsp -m 2 -c $i
+  iter=$(expr $total_iteration / $i)
+  mpirun -np $i ./build/bin/aco -f tests/xqf131.tsp -m 2 -c $i -n 200 -i $iter
   ./scripts/visualizer.py -f ./output/xqf131/xqf131_$i
 done
 ./scripts/profiler.py -f ./output/xqf131/xqf131_profile
